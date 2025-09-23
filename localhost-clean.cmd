1546cc23733ef1df1fad7f9f3933737046297e1a
Novimetrix <mohamed.elmalik@gmail.com>
2025-09-23 12:19:20 +0400

    235

--- localhost-clean.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set SCRIPT=%~dp0localhost-clean.ps1
-powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT%"
-echo.
-echo --- Report saved to "%CD%\localhost-clean.log" ---
-echo.
-pause
