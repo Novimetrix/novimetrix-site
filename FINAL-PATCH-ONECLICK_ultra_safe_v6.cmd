fb0b06087cadf145327d4802491fb673b017f059
Novimetrix <mohamed.elmalik@gmail.com>
2025-09-24 08:36:24 +0400

    klm

--- FINAL-PATCH-ONECLICK_ultra_safe_v6.cmd
+++ FINAL-PATCH-ONECLICK_ultra_safe_v6.cmd
@@ -1,10 +1,10 @@
 @echo off
-REM --- Novimetrix FINAL PATCH SAFE (double-click version) ---
+REM --- Novimetrix FINAL PATCH ULTRA SAFE v6 (double-click) ---
 cd /d %~dp0
-powershell -ExecutionPolicy Bypass -File ".\FINAL-PATCH-ONECLICK_safe.ps1" -Root .
+powershell -ExecutionPolicy Bypass -File ".\FINAL-PATCH-ONECLICK_ultra_safe_v6.ps1" -Root .
 echo.
 echo --------------------------------------------------------
-echo  FINAL-PATCH-ONECLICK (SAFE) has finished running.
+echo  FINAL-PATCH-ONECLICK (ULTRA SAFE v6) finished.
 echo  Log file saved as FINAL-PATCH.log in this folder.
 echo --------------------------------------------------------
 echo.
